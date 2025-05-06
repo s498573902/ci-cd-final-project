d15aa961ebff026920abbc672bd609cb0ee346f0
s498573902 <sdk567@gmail.com>
2025-05-06 15:14:25 -0400

    Correcting linting issue

--- service/common/log_handlers.py
+++ service/common/log_handlers.py
@@ -22,7 +22,6 @@ consistently
 """
 import logging
 
-
 def init_logging(app, logger_name: str):
     """Set up logging for production"""
     app.logger.propagate = False
@@ -31,7 +30,8 @@ def init_logging(app, logger_name: str):
     app.logger.setLevel(gunicorn_logger.level)
     # Make all log formats consistent
     formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
+        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", 
+        "%Y-%m-%d %H:%M:%S %z"
     )
     for handler in app.logger.handlers:
         handler.setFormatter(formatter)
